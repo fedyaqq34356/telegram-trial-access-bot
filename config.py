4d9bce2e3699ddfb4f3d7d7404ab49660f18fb75
fedya <fedya.3.4.6.2.7@gmail.com>
2026-01-05 12:14:26 +0200

    edit config.py

--- config.py
+++ config.py
@@ -6,12 +6,13 @@ class Config:
     bot_token: str
     work_chat_id: int
     study_group_id: int
-    trial_days: int = 8
+    trial_minutes: int = 11520
     
     @classmethod
     def from_env(cls):
         return cls(
             bot_token=os.getenv('BOT_TOKEN'),
             work_chat_id=int(os.getenv('WORK_CHAT_ID')),
-            study_group_id=int(os.getenv('STUDY_GROUP_ID'))
+            study_group_id=int(os.getenv('STUDY_GROUP_ID')),
+            trial_minutes=int(os.getenv('TRIAL_MINUTES', '11520'))
         )
